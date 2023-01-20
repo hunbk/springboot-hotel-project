ed71950656d1858fe3918b084019358de0d44e41
hunbk <shyun823@gmail.com>
2023-01-20 19:45:30 +0900

    refactor: 생성자를 `private`으로 변경

--- src/main/java/com/ptbh/kyungsunghotel/domain/board/BoardDto.java
+++ src/main/java/com/ptbh/kyungsunghotel/domain/board/BoardDto.java
@@ -1,25 +1,31 @@
 package com.ptbh.kyungsunghotel.domain.board;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Getter
-@Setter
 @NoArgsConstructor
-@AllArgsConstructor
 public class BoardDto {
 
     private Long id;
-    private Long memberId; //현재 게시글 뷰에서 loginMember랑 작성자를 비교하려고 사용. 리팩토링 대상.
+    private Long memberId; //TODO: 현재 게시글 뷰에서 loginMember랑 작성자를 비교하려고 사용. 리팩토링 대상.
     private String writer;
     private String title;
     private String content;
     private LocalDateTime createdDate;
     private LocalDateTime modifiedDate;
+
+    private BoardDto(Long id, Long memberId, String writer, String title, String content, LocalDateTime createdDate, LocalDateTime modifiedDate) {
+        this.id = id;
+        this.memberId = memberId;
+        this.writer = writer;
+        this.title = title;
+        this.content = content;
+        this.createdDate = createdDate;
+        this.modifiedDate = modifiedDate;
+    }
 
     public static BoardDto from(Board board) {
         return new BoardDto(
